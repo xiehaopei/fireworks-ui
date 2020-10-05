8c64c09d984540790a7d7d31ea6a1bcca1321375
xiehaopei <15907678645@163.com>
2020-10-05 21:34:36 +0800

    Home页面框架搭建

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <router-view></router-view>
+<router-view></router-view>
 </template>
 
 <script lang="ts">
-  export default {
-    name: 'App',
-  };
+export default {
+  name: "App",
+};
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,56 @@
+<!--
+ * @Author: Haopei Xie
+ * @Date: 2020-10-03 16:01:27
+ * @LastEditTime: 2020-10-05 21:33:00
+ * @LastEditors: Haopei Xie
+ * @Description: 
+ * @FilePath: \Pibukae:\vue\fireworks-ui\src\views\Home.vue
+-->
 <template>
-  <div>
-    <Topnav />
-    <div class="banner">
-      <h1>烟火UI</h1>
-      <h2>抓住转瞬即逝的美感</h2>
-      <p class="actions">
-        <a>GitHub</a>
-        <a>开始</a>
-      </p>
-    </div>
+<div>
+  <Topnav />
+  <div class="banner">
+    <h1>烟火UI</h1>
+    <h2>抓住转瞬即逝的美感</h2>
+    <p class="actions">
+      <a>GitHub</a>
+      <a>开始</a>
+    </p>
   </div>
+</div>
 </template>
 
 <script lang="ts">
-  import Topnav from '../components/Topnav.vue';
-  export default {
-    components: { Topnav },
-  };
+import Topnav from '../components/Topnav.vue';
+export default {
+  components: {
+    Topnav
+  },
+};
 </script>
+
+<style lang="scss" scoped>
+.banner {
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: lightgreen;
+
+  >.actions {
+    padding: 8px 0;
+
+    a {
+      margin: 0 8px;
+      background: #fff;
+      display: inline-block;
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 8px;
+    }
+  }
+}
+</style>
